829f23d1ed2341fb9594a9359c2b513986b8ba6f
Murilo Polese <murilopolese@gmail.com>
2018-09-25 20:03:10 +0100

    Rename app for rWindows purpose

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ install_requires = ['esptool==2.5.0', 'PyQt5==5.11.2', 'pyserial==3.4',
                     'appdirs==1.4.3']
 
 setup(
-    name='rpk-flash-tool',
+    name='rpkflashtool',
     version='0.1',
     description='A simple Flasher for Kano Pixel Kit.',
     long_description='Flash your Pixel Kit with MicroPython or Kano Code firmware.',
     author='Murilo Polese',
     author_email='murilo@kano.me',
-    url='https://github.com/murilopolese/rpk-flash-tool',
+    url='https://github.com/murilopolese/rpkflashtool',
     license='MIT',
     packages=['rpkflashtool'],
     install_requires=install_requires,
@@ -21,12 +21,12 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            "rpk-flash-tool = rpkflashtool.app:run",
+            "rpkflashtool = rpkflashtool.app:run",
         ],
     },
     options={  # Briefcase packaging options for OSX
         'app': {
-            'formal_name': 'rpk-flash-tool',
+            'formal_name': 'rpkflashtool',
             'bundle': 'me.kano',
         }
     }
